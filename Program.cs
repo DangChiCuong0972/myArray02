fc0c4adefdd5538fe68e417ad199e5b69687ba9b
hoanghaisc001 <69134435+hoanghaisc001@users.noreply.github.com>
2024-04-14 14:56:54 +0700

    Add Scripts
    Add Scripts

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.Contracts;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       string[] name = {"Lan","Giang","Nam","Chi","Linh","Thuan","Hoang"};
+
+       Console.WriteLine("Nhap mot ten bat ky");
+       
+       string a = Console.ReadLine();
+       bool check = false;
+       for (int i = 0; i < name.Length;i++)
+       {
+        if (Equals(name[i],a))
+        {
+           Console.WriteLine("Ten da nhap co trong danh sach");
+           check = true;
+        }
+       }
+
+       if(!check)
+       {
+        Console.WriteLine("Khong co tong danh sach");
+       }
+
+        
+
+    }
+
+}
